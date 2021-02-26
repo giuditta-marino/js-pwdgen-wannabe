53cb281ec13a9f16f481bfb09455f369d290ae3f
Giuditta <giudittamarino89@gmail.com>
2021-02-26 15:32:17 +0100

    Esercizio terminato

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+var nome = prompt("Qual è il tuo nome?");
+
+console.log(nome);
+
+var cognome = prompt("Qual è il tuo cognome?");
+
+console.log(cognome);
+
+var colorePreferito = prompt("Qual è il tuo colore preferito?");
+
+console.log(colorePreferito);
+
+document.getElementById('password').innerHTML= nome + cognome + colorePreferito + "21";

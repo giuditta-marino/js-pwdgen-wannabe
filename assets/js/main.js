48ccdc963f6a905cbe63721038123236365cf057
Giuditta <giudittamarino89@gmail.com>
2021-02-26 16:18:23 +0100

    esercizio perfezionato con commenti nel file js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,14 @@
+// CHIEDO NOME E SALVO IN VARIABILE
 var nome = prompt("Qual è il tuo nome?");
-
 console.log(nome);
 
+// CHIEDO COGNOME E SALVO IN VARIABILE
 var cognome = prompt("Qual è il tuo cognome?");
-
 console.log(cognome);
 
+// CHIEDO COLORE PREFERITO E SALVO IN VARIABILE
 var colorePreferito = prompt("Qual è il tuo colore preferito?");
-
 console.log(colorePreferito);
 
+// STAMPO PASSWORD NEL DOM NELL'ELEMENTO CON ID PASSWORD
 document.getElementById('password').innerHTML= nome + cognome + colorePreferito + "21";
